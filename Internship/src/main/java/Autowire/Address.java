ddb947252f3bceff472fe00abc5cf8f1888de6d2
DI4ME <128352141+DI4ME@users.noreply.github.com>
2026-05-19 10:23:34 +0530

    Add files via upload

---
+++ Internship/src/main/java/Autowire/Address.java
@@ -0,0 +1,15 @@
+package Autowire;
+
+public class Address {
+
+    private String city;
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return city;
+    }
+}
